3b1bd43993b635953a3c416d3313c4dbb6ae04e3
Maieras Andrei <maierasandrei@192.168.1.89>
2026-04-16 22:22:43 +0300

    v1.0.2 - Minor subtitle update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from PIL import Image, ImageOps, ImageFilter
 
 # ─── APP SETUP ────────────────────────────────────────────────────────────
 
-APP_VERSION = "1.0.1"
+APP_VERSION = "1.0.2"
 GITHUB_REPO = "amaieras/image-scraper"
 
 app = Flask(__name__)
