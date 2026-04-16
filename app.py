b075cd8837d42de2eeea775a69d3b4346691c585
Maieras Andrei <maierasandrei@192.168.1.89>
2026-04-16 21:47:41 +0300

    v1.4.0 - Update test v3

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from PIL import Image, ImageOps, ImageFilter
 
 # ─── APP SETUP ────────────────────────────────────────────────────────────
 
-APP_VERSION = "1.3.0"
+APP_VERSION = "1.4.0"
 GITHUB_REPO = "amaieras/image-scraper"
 
 app = Flask(__name__)
